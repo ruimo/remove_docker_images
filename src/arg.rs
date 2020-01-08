81e6ae8285cbc7b01ef03540637e4136c4bfcb57
Shisei Hanai <ruimo.uno@gmail.com>
2020-01-08 09:25:13 +0900

    Show version.

--- src/arg.rs
+++ src/arg.rs
@@ -2,19 +2,21 @@ extern crate clap;
 
 use clap::{App, Arg};
 use std::fmt;
+use super::VERSION;
 
 pub struct Args {
     pub is_dry_run: bool,
     pub keep_count: usize,
-    pub keep_count_snapshot: usize
+    pub keep_count_snapshot: usize,
+    pub show_version: bool,
 }
 
 impl fmt::Display for Args {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(
             f,
-            "Args(is_dry_run: {}, keep_count: {}, keep_count_snapshot: {})",
-            self.is_dry_run, self.keep_count, self.keep_count_snapshot
+            "Args(is_dry_run: {}, keep_count: {}, keep_count_snapshot: {}, show_version: {})",
+            self.is_dry_run, self.keep_count, self.keep_count_snapshot, self.show_version
         )
     }
 }
@@ -29,7 +31,7 @@ fn parse_int(s: &str, var_name: &str) -> usize {
     
 pub fn parse_arg() -> Args {
     let app = App::new("trimimages")
-        .version("0.1")
+        .version(VERSION)
         .author("Shisei Hanai<ruimo.uno@gmail.com>")
         .about("Remove docker images")
         .arg(Arg::with_name("dryrun")
@@ -45,13 +47,19 @@ pub fn parse_arg() -> Args {
              .help("Keep count for snapshot versioned image.")
              .long("keep-snapshot")
              .default_value("1")
-        );
+        )
+        .arg(Arg::with_name("show-version")
+             .help("Show version.")
+             .long("version")
+        )
+        ;
 
     let matches = app.get_matches();
 
     Args {
         is_dry_run: matches.is_present("dryrun"),
         keep_count: parse_int(matches.value_of("keep").unwrap(), "keep count"),
-        keep_count_snapshot: parse_int(matches.value_of("keep-snapshot").unwrap(), "keep count snapshot")
+        keep_count_snapshot: parse_int(matches.value_of("keep-snapshot").unwrap(), "keep count snapshot"),
+        show_version: matches.is_present("show-version"),
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,26 @@ mod arg;
 mod image;
 mod version;
 
+const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
 fn main() {
     let args: arg::Args = arg::parse_arg();
-    println!("args {}", args);
 
-    let images = image::perform();
-    images.delete(args.keep_count, args.keep_count_snapshot, |repo, ver| {
-        if args.is_dry_run {
-            println!("delete {}:{}", repo, ver);
-        } else {
-            let img = format!("{}:{}", repo, ver);
-            Command::new("docker")
-                .arg("rmi")
-                .arg(img)
-                .output()
-                .expect("Cannot run 'docker rmi {}'. Please check docker installation.");
-        }
-    });
+    if args.show_version {
+        println!("{}", VERSION);
+    } else {
+        let images = image::perform();
+        images.delete(args.keep_count, args.keep_count_snapshot, |repo, ver| {
+            if args.is_dry_run {
+                println!("delete {}:{}", repo, ver);
+            } else {
+                let img = format!("{}:{}", repo, ver);
+                Command::new("docker")
+                    .arg("rmi")
+                    .arg(img)
+                    .output()
+                    .expect("Cannot run 'docker rmi {}'. Please check docker installation.");
+            }
+        });
+    }
 }
